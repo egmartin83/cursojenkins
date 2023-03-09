1183d0598e35d857f087a208da2d3977d33a006b
egmartin83 <egmartin83@gmail.com>
2023-03-09 20:44:17 -0300

    Create jobDSL.groovy

---
+++ JenkinsJobDSL/jobDSL.groovy
@@ -0,0 +1,39 @@
+job('ejemplo-DSL-desde-git'){
+		scm {
+        git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('egmartin83')
+            node / gitConfigEmail('egmartin83@gmail.com')
+        }
+    }
+		parameters{
+			stringParam('nombre', defaultValue = 'Emiliano del Valle',description = 'Parametro de nombre')
+			choiceParam('planeta',['Tierra','Venus','Mercurio','Saturno','Jupiter'])
+			booleanParam('agente',false)
+		}
+		triggers{
+			cron('H/7 * * * *')
+		}
+		steps {
+	    shell("bash jobscript.sh")
+	  }
+		publishers {
+	    mailer('macloujulian@gmail.com', true, true)
+	    slackNotifier {
+	      notifyAborted(true)
+	      notifyEveryFailure(true)
+	      notifyNotBuilt(false)
+	      notifyUnstable(false)
+	      notifyBackToNormal(true)
+	      notifySuccess(false)
+	      notifyRepeatedFailure(false)
+	      startNotification(false)
+	      includeTestSummary(false)
+	      includeCustomMessage(false)
+	      customMessage(null)
+	      sendAs(null)
+	      commitInfoChoice('NONE')
+	      teamDomain(null)
+	      authToken(null)
+	    }
+	  }
+}
